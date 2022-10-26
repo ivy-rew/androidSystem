d915963b5af038ff4fbb790ca23f57abad7dd84c
Lord eAgle <r.wermelinger@gmail.com>
2022-10-26 21:13:22 +0200

    help and data keep toogle

--- uninstallApps.sh
+++ uninstallApps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+uninst=""
 uninstall() {
   app=$1
   read -p "Delete app '${app}' ? [y|n]"
   if [[ $REPLY == "y" ]]; then
     echo "killing ${app}"
-    cmd="pm uninstall -k --user 0 ${app}"
+    cmd="pm uninstall${uninst} --user 0 ${app}"
     echo "running command on device: '${cmd}'"
     adb shell -t "${cmd}"
   else
@@ -13,8 +14,14 @@ uninstall() {
   fi
 }
 
+list=""
 listPkgs() {
-  cmd="pm list packages -f"
+  cmd="pm list packages ${list}"
+  adb shell "${cmd}"
+}
+
+pkg() {
+  cmd="pm"
   adb shell "${cmd}"
 }
 
@@ -26,13 +33,24 @@ uninstallMenu() {
     apps=$(listPkgs | grep $filter)
   fi
 
-  OPTIONS=( '!exit' '!filter' '!google' ${apps[@]} )
+  OPTIONS=( '!exit' '!filter' '!pkg:help' '!list:full' '!data:keep' '!google' ${apps[@]} )
   echo "SELECT app to uninstall"
   select OPTION in ${OPTIONS[@]}; do
     if [ "$OPTION" == "!exit" ]; then
       break
     elif [ "$OPTION" == "!google" ]; then
       uninstallMenu "google"
+      break
+    elif [ "$OPTION" == "!pkg:help" ]; then
+      pkg
+      break
+    elif [ "$OPTION" == "!data:keep" ]; then
+      uninst="${uninst} -k"
+      uninstallMenu "$filter"
+      break
+    elif [ "$OPTION" == "!list:full" ]; then
+      list="${list} -f"
+      uninstallMenu "$filter"
       break
     elif [ "$OPTION" == "!filter" ]; then
       read -p "Search for what? "
