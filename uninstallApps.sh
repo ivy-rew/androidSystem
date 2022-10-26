43dfa97c071fc176fc39d7c3bac6401f9747f504
Lord eAgle <r.wermelinger@gmail.com>
2022-10-26 19:40:20 +0200

    user input pkg filter via ui

--- uninstallApps.sh
+++ uninstallApps.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
 uninstall() {
-    app=$1
-    read -p "Delete app ${app}? [y|n]"
-    if [[ $REPLY == "y" ]]; then
-        echo "killing ${app}"
-        cmd="pm uninstall -k --user 0 ${app}"
-        echo "running command on device: '${cmd}'"
-        adb shell -t "${cmd}"
-    else
-        echo "aborted removal off ${app}."
-    fi
+  app=$1
+  read -p "Delete app '${app}' ? [y|n]"
+  if [[ $REPLY == "y" ]]; then
+    echo "killing ${app}"
+    cmd="pm uninstall -k --user 0 ${app}"
+    echo "running command on device: '${cmd}'"
+    adb shell -t "${cmd}"
+  else
+    echo "aborted removal off ${app}."
+  fi
+}
+
+listPkgs() {
+  cmd="pm list packages -f"
+  adb shell "${cmd}"
 }
 
 uninstallMenu() {
   filter=$1
-  apps=$(adb shell 'pm list packages -f')
+  apps=$(listPkgs)
   if [[ ! -z "$filter" ]]; then
     echo "filtering ${filter}"
-    apps=$(adb shell 'pm list packages -f' | grep $filter)
+    apps=$(listPkgs | grep $filter)
   fi
 
-  OPTIONS=( '!exit' '!google' '!huawei' ${apps[@]} )
+  OPTIONS=( '!exit' '!filter' '!google' ${apps[@]} )
   echo "SELECT app to uninstall"
   select OPTION in ${OPTIONS[@]}; do
     if [ "$OPTION" == "!exit" ]; then
-        break
+      break
     elif [ "$OPTION" == "!google" ]; then
-        uninstallMenu "google"
-        break
-    elif [ "$OPTION" == "!huawei" ]; then
-        uninstallMenu "huawei"
-        break
+      uninstallMenu "google"
+      break
+    elif [ "$OPTION" == "!filter" ]; then
+      read -p "Search for what? "
+      uninstallMenu "$REPLY"
+      break
     else
-        app=$(echo "${OPTION}" | sed 's/.*=//')
-        uninstall "${app::-1}"
+      app=$(echo "${OPTION}" | sed 's/.*=//')
+      uninstall "${app}"
     fi
   done
 }
