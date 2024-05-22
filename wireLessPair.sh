1dc90abd00ac20e855537488b156620d20fde6a2
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-05-22 20:23:14 +0200

    device code works better on redmi:android14 than QR

--- wireLessPair.sh
+++ wireLessPair.sh
@@ -11,6 +11,22 @@ PATH=$PWD/platform-tools:$PATH
 # stop previous attempts to pure tcp/ip connect
 adb kill-server
 
-# https://github.com/saleehk/adb-wifi
-npm i adb-wifi
-./node_modules/adb-wifi/cli.js
+qrCode() {
+  # https://github.com/saleehk/adb-wifi
+  npm i adb-wifi
+  ./node_modules/adb-wifi/cli.js
+}
+
+deviceCode() {
+  echo "To see your IP on Android; you may disable VPN clients before debugging"
+  read -p "IP Address to connect: " ip
+  echo "Enter the Port seen in DeveloperOptions > Wifi Debugging > IP Address & Port (not the Device pair pop-up port)"
+  read -p "Port to connect: " port
+  echo "connecting to: $ip:$port"
+  echo "Now open DeveloperOptions > Wifi Debugging > Pair device with pairing code ... enter the in this shell"
+  adb pair $ip:$port
+  adb connect $ip:$port
+  adb devices
+}
+
+deviceCode
